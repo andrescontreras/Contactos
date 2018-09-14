698becf19e2336d8bb137bf7aa3520b116ccc932
Andres Contreras <andres_c8@outlook.com>
2018-09-14 15:44:00 -0500

    agregar contactos nueva publicacion

--- src/app/components/n-publicacion/n-publicacion.component.ts
+++ src/app/components/n-publicacion/n-publicacion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ConstantPool } from '../../../../node_modules/@angular/compiler/src/constant_pool';
 import { Contacto, T_Contacto } from '../../clases/contacto';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -15,13 +15,18 @@ export class NPublicacionComponent implements OnInit {
 
   //contacto:Contacto = new Contacto("","","");
   formasc =  false;
-  tcontacto:string;
+  tcontacto:string = "Seleccione uno";
   contacto:string = "";
+
+  edittcontacto:string = "Seleccione uno";
+  editcontacto:string = "";
+  editIndex:number = -1;
+  @ViewChild('btnClose') btnClose :ElementRef;
   
   lista = [
-    new T_Contacto("celular", "5464654"),
-    new T_Contacto("celular", "11111"),
-    new T_Contacto("celular", "1111")
+    new T_Contacto("Telefono movil", "5464654"),
+    new T_Contacto("Telefono movil", "11111"),
+    new T_Contacto("Telefono movil", "1111")
   ];
 
   constructor(
@@ -39,9 +44,30 @@ export class NPublicacionComponent implements OnInit {
     this.formasc = !this.formasc;
   }
 
+  editContacto(i)
+  {
+    console.log("contacto editado ES: ",i);
+    console.log( this.lista[i])
+    this.editcontacto = this.lista[i].contacto
+    this.edittcontacto = this.lista[i].tipo
+    this.editIndex = i;
+  }
+
+  confirmEdit(){
+    this.lista.splice(this.editIndex,1,new T_Contacto(this.edittcontacto, this.editcontacto) );
+    this.btnClose.nativeElement.click();
+  }
+
+  deleteContacto(i)
+  {
+    console.log("contacto eliminado ES: ",i);
+    this.lista.splice(i)
+  }
   addContacto()
   {
     this.lista.push(new T_Contacto(this.tcontacto, this.contacto) );
+    this.contacto = "";
+    this.tcontacto = "Seleccione uno"
   }
 
   submit()
